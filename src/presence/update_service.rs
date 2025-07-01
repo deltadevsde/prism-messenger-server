4ab7db06431d0f2dbe9d7b5e0371f333036f6e5b
Jonas <jonas@deltadevs.xyz>
2025-07-01 11:06:32 +0200

    refactor(presence): remove obsolete methods

--- src/presence/update_service.rs
+++ src/presence/update_service.rs
@@ -1,12 +1,6 @@
 use std::sync::Arc;
-use tracing::instrument;
-use uuid::Uuid;
 
-use super::{
-    entities::PresenceStatus,
-    error::PresenceError,
-    gateway::{PresenceGateway, PresenceUpdate},
-};
+use super::gateway::PresenceGateway;
 
 pub struct PresenceUpdateService<G>
 where
@@ -23,113 +17,90 @@ where
         Self { presence_gateway }
     }
 
-    #[instrument(skip(self))]
-    pub async fn handle_connection_established(
-        &self,
-        account_id: Uuid,
-    ) -> Result<(), PresenceError> {
-        let presence_update = PresenceUpdate::new(account_id, PresenceStatus::Online);
-        self.presence_gateway
-            .send_presence_update(&presence_update)
-            .await?;
-        Ok(())
-    }
-
-    #[instrument(skip(self))]
-    pub async fn handle_connection_closed(&self, account_id: Uuid) -> Result<(), PresenceError> {
-        let presence_update = PresenceUpdate::new(account_id, PresenceStatus::Offline);
-        self.presence_gateway
-            .send_presence_update(&presence_update)
-            .await?;
-        Ok(())
-    }
-
+    #[tracing::instrument(skip(self))]
     pub async fn handle_presence_updates(&self) {
-        {
-            let gateway = self.presence_gateway.clone();
-            self.presence_gateway
-                .register_presence_handler(move |presence_update| {
-                    let gateway = gateway.clone();
-                    tokio::spawn(async move {
-                        if let Err(e) = gateway.send_presence_update(&presence_update).await {
-                            tracing::error!(
-                                error = %e,
-                                "Failed to send presence update"
-                            );
-                        }
-                    });
-                })
-                .await;
-        }
+        let gateway = self.presence_gateway.clone();
+        self.presence_gateway
+            .register_presence_handler(move |presence_update| {
+                let gateway = gateway.clone();
+                tokio::spawn(async move {
+                    if let Err(e) = gateway.send_presence_update(&presence_update).await {
+                        tracing::error!(
+                            error = %e,
+                            "Failed to send presence update"
+                        );
+                    }
+                });
+            })
+            .await;
     }
 }
 
 #[cfg(test)]
 mod tests {
     use super::*;
-    use crate::presence::gateway::MockPresenceGateway;
+    use crate::presence::entities::PresenceStatus;
+    use crate::presence::error::PresenceError;
+    use crate::presence::gateway::{MockPresenceGateway, PresenceUpdate};
+    use mockall::predicate::*;
+    use tokio::sync::mpsc;
+    use uuid::Uuid;
 
     #[tokio::test]
-    async fn test_handle_connection_established() {
-        let account_id = Uuid::new_v4();
-
+    async fn test_handle_presence_updates() {
         let mut mock_gateway = MockPresenceGateway::new();
+        let (tx, mut rx) = mpsc::channel(1);
+
+        mock_gateway
+            .expect_register_presence_handler()
+            .with(always())
+            .times(1)
+            .returning(move |handler| {
+                let tx = tx.clone();
+                tokio::spawn(async move {
+                    let update = PresenceUpdate::new(Uuid::new_v4(), PresenceStatus::Online);
+                    handler(update);
+                    tx.send(()).await.unwrap();
+                });
+            });
+
         mock_gateway
             .expect_send_presence_update()
+            .with(always())
             .times(1)
             .returning(|_| Ok(()));
 
         let service = PresenceUpdateService::new(Arc::new(mock_gateway));
-
-        let result = service.handle_connection_established(account_id).await;
-        assert!(result.is_ok());
+        service.handle_presence_updates().await;
+        rx.recv().await.unwrap();
     }
 
     #[tokio::test]
-    async fn test_handle_connection_closed() {
-        let account_id = Uuid::new_v4();
-
+    async fn test_handle_presence_updates_error() {
         let mut mock_gateway = MockPresenceGateway::new();
+        let (tx, mut rx) = mpsc::channel(1);
+
+        mock_gateway
+            .expect_register_presence_handler()
+            .with(always())
+            .times(1)
+            .returning(move |handler| {
+                let tx = tx.clone();
+                tokio::spawn(async move {
+                    let update = PresenceUpdate::new(Uuid::new_v4(), PresenceStatus::Online);
+                    handler(update);
+                    tx.send(()).await.unwrap();
+                });
+            });
+
         mock_gateway
             .expect_send_presence_update()
+            .with(always())
             .times(1)
-            .returning(|_| Ok(()));
+            .returning(|_| Err(PresenceError::SendingFailed("test error".to_string())));
 
         let service = PresenceUpdateService::new(Arc::new(mock_gateway));
-
-        let result = service.handle_connection_closed(account_id).await;
-        assert!(result.is_ok());
-    }
-
-    #[tokio::test]
-    async fn test_handle_connection_established_gateway_error() {
-        let account_id = Uuid::new_v4();
-
-        let mut mock_gateway = MockPresenceGateway::new();
-        mock_gateway
-            .expect_send_presence_update()
-            .times(1)
-            .returning(|_| Err(PresenceError::SendingFailed("Test error".to_string())));
-
-        let service = PresenceUpdateService::new(Arc::new(mock_gateway));
-
-        let result = service.handle_connection_established(account_id).await;
-        assert!(result.is_err());
-    }
-
-    #[tokio::test]
-    async fn test_handle_connection_closed_gateway_error() {
-        let account_id = Uuid::new_v4();
-
-        let mut mock_gateway = MockPresenceGateway::new();
-        mock_gateway
-            .expect_send_presence_update()
-            .times(1)
-            .returning(|_| Err(PresenceError::SendingFailed("Test error".to_string())));
-
-        let service = PresenceUpdateService::new(Arc::new(mock_gateway));
-
-        let result = service.handle_connection_closed(account_id).await;
-        assert!(result.is_err());
+        service.handle_presence_updates().await;
+        rx.recv().await.unwrap();
     }
 }
